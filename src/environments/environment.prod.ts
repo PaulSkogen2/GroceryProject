f0e01eea4fbcaafa3d91edfa00b359ff60dee4ed
PSkogs22 <paul.skogen2@gmail.com>
2021-01-12 21:16:50 -0600

    Initial environment buildout and connecting to Firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyB1-d6CSt1KPgjx_Xtk71J116fK-JLMm5I",
+    authDomain: "oshop-c9dcd.firebaseapp.com",
+    projectId: "oshop-c9dcd",
+    storageBucket: "oshop-c9dcd.appspot.com",
+    messagingSenderId: "944211993767",
+    appId: "1:944211993767:web:56da69c7b48dccdc6aa782",
+    measurementId: "G-BFHR0L79JT"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyB1-d6CSt1KPgjx_Xtk71J116fK-JLMm5I",
+    authDomain: "oshop-c9dcd.firebaseapp.com",
+    projectId: "oshop-c9dcd",
+    storageBucket: "oshop-c9dcd.appspot.com",
+    messagingSenderId: "944211993767",
+    appId: "1:944211993767:web:56da69c7b48dccdc6aa782",
+    measurementId: "G-BFHR0L79JT"
+  }
 };
 
 /*
